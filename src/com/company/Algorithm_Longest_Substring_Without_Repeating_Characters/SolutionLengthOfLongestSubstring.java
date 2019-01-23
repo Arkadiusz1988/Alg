98f7266eb6c482090ae0ccb448010d43c2f52d04
Arkadiusz Hojenski <arkadiusz.hojenski@gmail.com>
2019-01-23 14:34:07 +0100

    smallest positive integer algorithm added

---
+++ src/com/company/Algorithm_Longest_Substring_Without_Repeating_Characters/SolutionLengthOfLongestSubstring.java
@@ -0,0 +1,22 @@
+package com.company.Algorithm_Longest_Substring_Without_Repeating_Characters;
+
+import java.util.*;
+
+public class SolutionLengthOfLongestSubstring {
+
+    public static int lengthOfLongestSubstring(String s) {
+
+        if (s.length()==0 || s.trim().length()==0){
+            return 0;
+        }
+
+        if (s.length()==1){
+            return 1;
+        }
+//        if (s.length()==2){
+//            return 2;
+//        }
+
+        return 1;
+    }
+}
